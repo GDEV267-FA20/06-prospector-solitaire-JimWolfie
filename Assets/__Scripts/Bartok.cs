8ea92d07b8dfd5d2c44a0c2e1fc33037e4a354a1
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-10-25 18:56:53 -0400

    set up bartok and scenes some more. prelayout

---
+++ Assets/__Scripts/Bartok.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bartok : MonoBehaviour
+{
+    static public Bartok S;
+
+    [Header("set in inspector")]
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
+    public Vector3 layoutCenter = Vector3.zero;
+
+    [Header("set dynamically")]
+    public Deck deck;
+    public List<CardBartok> drawPile;
+    public List<CardBartok> discardPile;
+
+    private void Awake()
+    {
+        S=this;
+    }
+    private void Start()
+    {
+        deck = GetComponent<Deck>();
+        deck.InitDeck(deckXML.text);
+        Deck.Shuffle(ref deck.cards);
+    }
+}
